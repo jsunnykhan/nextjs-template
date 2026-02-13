19f6fb41e5466d18b64ef11972635bd8e6976378
Sunny Khan <24373594+jsunnykhan@users.noreply.github.com>
2026-02-14 01:25:37 +0600

    fix: biome config

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,24 +1,27 @@
-import LoginPage from '@/features/auth/login';
-import { auth } from '@/lib/auth';
-import { Metadata } from 'next';
-import { headers } from 'next/headers';
-import { redirect } from 'next/navigation';
+import type { Metadata } from "next";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import LoginPage from "@/features/auth/login";
+import { auth } from "@/lib/auth";
+
 
 export const metadata: Metadata = {
-  title: 'Login',
-  description: 'Login Page',
+	title: "Login",
+	description: "Login Page",
 };
 
-const page = async() => {
-  const session = await auth.api.getSession({headers:await headers()})
+const page = async () => {
+	const session = await auth.api.getSession({ headers: await headers() });
 
-  if (session){
-    redirect("/")
-  }
+	if (session) {
+		redirect("/");
+	}
 
-  return <main className='flex min-h-svh items-center justify-center p-6 md:p-10'>
-    <LoginPage />
-  </main>;
+	return (
+		<main className="flex min-h-svh items-center justify-center p-6 md:p-10">
+			<LoginPage />
+		</main>
+	);
 };
 
 export default page;
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,56 +1,56 @@
-'use client';
+"use client";
 
-import { authClient } from '@/lib/auth-client';
-import { useState } from 'react';
+import { useState } from "react";
+import { authClient } from "@/lib/auth-client";
+import { Button } from "./ui/button";
 
 const Header = () => {
+	const { data: session } = authClient.useSession();
 
-  const {data: session} = authClient.useSession()
+	const [open, setOpen] = useState<boolean>(false);
 
-  const [open, setOpen] = useState<boolean>(false);
+	return (
+		<header className="min-h-[64px] px-6 flex items-center justify-between bg-gray-100 text-gray-800 shadow-sm">
+			<div className="relative">
+				<Button
+					onClick={() => setOpen(!open)}
+					className="flex items-center gap-2 px-4 py-2 rounded hover:bg-gray-200 transition"
+				>
+					<span>{session?.user?.name}</span>
+					{/** biome-ignore lint/a11y/noSvgWithoutTitle: <explanation> */}
+					<svg
+						className="w-4 h-4"
+						fill="none"
+						stroke="currentColor"
+						strokeWidth="2"
+						viewBox="0 0 24 24"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M19 9l-7 7-7-7"
+						/>
+					</svg>
+				</Button>
 
-
-  return (
-    <header className="min-h-[64px] px-6 flex items-center justify-between bg-gray-100 text-gray-800 shadow-sm">
-      <div className="relative">
-        <button
-          onClick={() => setOpen(!open)}
-          className="flex items-center gap-2 px-4 py-2 rounded hover:bg-gray-200 transition"
-        >
-          <span>{session?.user?.name}</span>
-          <svg
-            className="w-4 h-4"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            viewBox="0 0 24 24"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M19 9l-7 7-7-7"
-            />
-          </svg>
-        </button>
-
-        {/* Dropdown */}
-        {open && (
-          <div className="absolute right-0 mt-2 w-48 bg-white rounded shadow-md z-10 border border-gray-200">
-            <div className="px-4 py-2 text-sm text-gray-600">
-              {session?.user.email}
-            </div>
-            <hr className="border-gray-200" />
-            <p className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer">
-              Profile
-            </p>
-            <p className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer">
-              Sign out
-            </p>
-          </div>
-        )}
-      </div>
-    </header>
-  );
+				{/* Dropdown */}
+				{open && (
+					<div className="absolute right-0 mt-2 w-48 bg-white rounded shadow-md z-10 border border-gray-200">
+						<div className="px-4 py-2 text-sm text-gray-600">
+							{session?.user.email}
+						</div>
+						<hr className="border-gray-200" />
+						<p className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer">
+							Profile
+						</p>
+						<p className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer">
+							Sign out
+						</p>
+					</div>
+				)}
+			</div>
+		</header>
+	);
 };
 
 export default Header;
